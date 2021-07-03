d90d0ef90563780e8bd34ed1cfac322d9d38193f
codej625 <codej625@gmail.com>
2021-07-03 23:24:29 +0900

    210703

---
+++ och04_sdlc/src/main/java/sdlc01/MainClass.java
@@ -0,0 +1,28 @@
+package sdlc01;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+
+		System.out.println("1 ctx.load Before");
+		
+		ctx.load("classpath:applicationCTX01.xml");
+		
+		System.out.println("2 ctx.load After");
+		
+		// 생성자 관련 init
+		ctx.refresh();
+		
+		System.out.println("3 ctx.refresh After");
+		
+		// 소멸자 관련 destroy
+		ctx.close();
+		System.out.println("4 ctx.close After");
+
+	}
+
+}
